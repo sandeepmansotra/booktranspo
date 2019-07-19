a1edf2ab30684a91b2dc2d06f4e53e4046459312
Sandeep Mansotra <sandeepsharma1998.sp@gmail.com>
2019-07-19 15:05:06 +0530

    sample template

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="main-footer">
+          <strong>
+            Copyright ©2019 <a href="#">BookTranspo</a>.
+          </strong>{" "}
+          All rights reserved.
+        </footer>
+      </div>
+    );
+  }
+}
